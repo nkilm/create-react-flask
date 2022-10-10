16b7fa9a3c4255a97a005a241a3b1bd93eb40492
sreekeshiyer <sreekesh.subramanian@gmail.com>
2022-10-10 17:57:37 +0530

    🐛 ✨ Modified Script to delete existing Git Repository on command execution.

--- bin/cli.js
+++ bin/cli.js
@@ -1,22 +1,23 @@
 #!/usr/bin/env node
 
-const {execSync} = require('child_process');
+const { execSync } = require("child_process");
 
 const URL = "https://github.com/nkilm/create-react-flask";
 const repositoryName = process.argv[2];
 
-const runCommand = cmd =>{
-    try{
-        execSync(cmd,{stdio: 'inherit'});
-    } catch (err){
-        console.log(`Failed to execute ${cmd}`,err);
+const runCommand = (cmd) => {
+    try {
+        execSync(cmd, { stdio: "inherit" });
+    } catch (err) {
+        console.log(`Failed to execute ${cmd}`, err);
         return false;
     }
     return true;
-}
+};
 
 const gitCloneCommand = `git clone --depth 1 ${URL} ${repositoryName}`;
 const installDepCommand = `cd ${repositoryName} && npm install`;
+const gitCleanupCommand = `cd ${repositoryName} && rm -rf .git`;
 
 console.log(`Cloning the repository with name ${repositoryName}`);
 
@@ -26,5 +27,10 @@ if (!commandOutput) process.exit(1);
 const installDepOutput = runCommand(installDepCommand);
 if (!installDepOutput) process.exit(1);
 
-console.log(`Congratulations! You are ready. Follow the following commands to start`);
+const gitCleanupOutput = runCommand(gitCleanupCommand);
+if (!gitCleanupOutput) process.exit(1);
+
+console.log(
+    `Congratulations! You are ready. Follow the following commands to start`
+);
 console.log(`cd ${repositoryName} && npm start`);
